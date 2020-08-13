84c6f3b979d8b2aff72b7573e4332f4ce00dd503
jurgenru <jurgenruegnberg@gmail.com>
2020-08-13 12:36:31 -0400

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { AppComponent } from './app.component';
 //aqui le cambie el nombre de la carpeta antes decia test de como lo creamos la primera vez con el profesor
 import { TestComponent } from './components/test.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaComponent } from './components/persona/persona.component';
+import { PersonaModule } from './modules/persona/persona.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    PersonaComponent
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    PersonaModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
