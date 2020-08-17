a4f5afdc92d443eaa84a6d9dda0bc4c9f9a6bc20
jurgenru <jurgenruegnberg@gmail.com>
2020-08-17 15:48:37 -0400

    MouseEnter, MouseLeave

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,13 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
-  constructor(el: ElementRef) {
+  /*Escuchar un evento en donde este declarado. Escucha cualquier elemento o componente donde este este elemento. EN este ejemplo vamos a escuchar lo del mouse que esta haciendo */
+  @HostListener('mouseenter') onMouseEnter(){
+    console.log('Mouse enter')
+    this.changeCOlor('green');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    console.log('Mouse leave')
+    this.changeCOlor('grey')
+  }
+
+  constructor(private el: ElementRef) {
     console.log('Element ref: ', el)
-    el.nativeElement.style.backgroundColor = 'red';
+    this.changeCOlor('red')
+  }
+
+  changeCOlor(color:string){
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
