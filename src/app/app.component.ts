892a456551080e2652e6e2d383da1c418860e4f7
jurgenru <jurgenruegnberg@gmail.com>
2020-08-20 00:25:57 -0400

    intentoDeArregloDespuesDELEXAMEN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,28 @@ export class AppComponent implements OnInit {
     }
   ];
 
+  restaurants = [
+  {
+    nombre : 'khao soi', stock:1, precio : 20, tipo : 'internacional'},
+  {
+  nombre : "sajta ", stock: 2, precio : 42, tipo : "nacional"},
+  {
+  nombre : "semla ", stock: 0, precio : 52, tipo : "internacional"},
+  {
+  nombre : "silpancho", stock: 45, precio : 5, tipo : "nacional"},
+  {
+  nombre : "pulao", stock: 20, precio : 41, tipo : "internacional"},
+  {
+  nombre : "saice ", stock: 1, precio : 7, tipo : "nacional"},
+  {
+  nombre : "poutine", stock: 4, precio : 10, tipo : "internacional"},
+  {
+  nombre : "chicharron", stock: 0, precio : 4, tipo : "nacional"},
+  {
+  nombre : "fricase ", stock: 33, precio : 47, tipo : "nacional"},
+  {
+  nombre : "sushi", stock: 75, precio : 5, tipo : "internacional"},
+  ]
   ngOnInit(){
     /*
     funcion flecha o arrow function
