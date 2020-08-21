df4e30d47fcc54bdca7e0c43470fc5da0881b233
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-21 15:52:59 -0400

    first child router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
 import {AppRouting} from './app.routing';
+import { Home1Component } from './components/home1/home1.component';
+import { Home2Component } from './components/home2/home2.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +32,9 @@ import {AppRouting} from './app.routing';
     NgFocusDirective,
     HomeComponent,
     AboutComponent,
-    AdminComponent
+    AdminComponent,
+    Home1Component,
+    Home2Component
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {AdminComponent} from './components/admin/admin.component';
+import {Home1Component} from './components/home1/home1.component';
+import {Home2Component} from './components/home2/home2.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {path: 'home1', component: Home1Component},
+      {path: 'home2', component: Home2Component}
+    ]
+  },
   {path: 'about', component: AboutComponent},
   {path: 'admin', component: AdminComponent}
 ];
