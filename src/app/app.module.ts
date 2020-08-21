ff98a846cf737d08012d71156a6d23b895cdec39
jurgenru <jurgenruegnberg@gmail.com>
2020-08-21 19:39:58 -0400

    dishmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ComidaComponent } from './modules/comida/components/comida/comida.component';
+import { Dish1Component } from './dish1/dish1.component';
+import { Dish2Component } from './dish2/dish2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComidaComponent
+    ComidaComponent,
+    Dish1Component,
+    Dish2Component
   ],
   imports: [
     BrowserModule
