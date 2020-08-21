a0f8ff9f61973a62b74925c6c1350ae5f798a430
jurgenru <jurgenruegnberg@gmail.com>
2020-08-21 19:56:03 -0400

    dishAndProviderModules

---
+++ src/app/MODULESEX/SharedModule/ng-shared2.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appNgShared2]'
+})
+export class NgShared2Directive {
+
+  constructor() { }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ComidaComponent } from './modules/comida/components/comida/comida.component';
-import { Dish1Component } from './dish1/dish1.component';
-import { Dish2Component } from './dish2/dish2.component';
+import { Dish1Component } from './MODULESEX/DishModule/dish/components/dish1/dish1.component';
+import { Dish2Component } from './MODULESEX/DishModule/dish/components/dish2/dish2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComidaComponent,
-    Dish1Component,
-    Dish2Component
+   Dish1Component,
+   Dish2Component
   ],
   imports: [
     BrowserModule
