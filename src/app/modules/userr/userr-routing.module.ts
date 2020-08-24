6acb487e216731e2bc06b4c36a2da342f89d59e8
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-24 15:11:46 -0400

    modules routing with children

--- src/app/modules/userr/userr-routing.module.ts
+++ src/app/modules/userr/userr-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UserComponent} from './user/user.component';
+import {User1Component} from './user1/user1.component';
+import {User2Component} from './user2/user2.component';
 
-//'' significa que el componente que tengamos va a correr sin problemas no importa el path
+// '' significa que el componente que tengamos va a correr sin problemas no importa el path
 const routes: Routes = [
-  {path: '', component: UserComponent}
+  {
+    path: '',
+    component: UserComponent,
+    children: [
+      {path: 'user1', component: User1Component},
+      {path: 'user2', component: User2Component}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/modules/userr/userr.module.ts
+++ src/app/modules/userr/userr.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserrRoutingModule } from './userr-routing.module';
 import { UserComponent } from './user/user.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [
+    UserComponent,
+    User1Component,
+    User2Component],
   imports: [
     CommonModule,
     UserrRoutingModule
