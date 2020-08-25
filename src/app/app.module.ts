8d7ca77f99880e0d9c6184b5516f6cc0e49ea284
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-25 00:36:58 -0400

    ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { AdminComponent } from './components/admin/admin.component';
 import {AppRouting} from './app.routing';
 import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,7 +45,8 @@ import { Home2Component } from './components/home2/home2.component';
     ProductoModule,
     AdminModule,
     UserModule,
-    AppRouting
+    AppRouting,
+    NgbModule
   ],
   providers: [
 
