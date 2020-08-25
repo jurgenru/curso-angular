3aa2f2d77c41131db8a396b9eef6d7332ad0f2db
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-25 16:54:25 -0400

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,51 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaModule } from './modules/persona/persona.module';
-import { ProductoModule } from './modules/producto/producto.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
-import { MipipePipe } from './pipes/mipipe.pipe';
-import { ExpoPipe } from './pipes/expo.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { NgColorDirective } from './directives/ng-color.directive';
-import { NgFocusDirective } from './directives/ng-focus.directive';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
 import {AppRouting} from './app.routing';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent,
-    MipipePipe,
-    ExpoPipe,
-    PurePipe,
-    ImpurePipe,
-    NgColorDirective,
-    NgFocusDirective,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    PersonaModule,
-    ProductoModule,
-    AdminModule,
-    UserModule,
     AppRouting,
     NgbModule,
 
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
+/*import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {AdminComponent} from './components/admin/admin.component';
 import {Home1Component} from './components/home1/home1.component';
-import {Home2Component} from './components/home2/home2.component';
+import {Home2Component} from './components/home2/home2.component';*/
 
 const routes: Routes = [
-  {
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path : 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path : 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
+  {path : 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)}
+  /*{
     path: 'home',
     component: HomeComponent,
     children: [
@@ -17,7 +21,7 @@ const routes: Routes = [
   },
   {path: 'about', component: AboutComponent},
   {path: 'admin', component: AdminComponent},
-  {path: 'userr', loadChildren: () => import('./modules/userr/userr.module').then(m => m.UserrModule)}
+  {path: 'userr', loadChildren: () => import('./modules/userr/userr.module').then(m => m.UserrModule)}*/
 ];
 
 /**forChild son rutas niños se colocan cuando de un padre salen sus niños subrutas de una vista
