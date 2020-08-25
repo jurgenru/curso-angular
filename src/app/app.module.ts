95a8c1137df07db9bf03e3fc84776f3fb6c48c2c
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-25 16:04:43 -0400

    angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import {AppRouting} from './app.routing';
 import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -46,7 +48,10 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     AdminModule,
     UserModule,
     AppRouting,
-    NgbModule
+    NgbModule,
+
+    MatSliderModule,
+    MatIconModule
   ],
   providers: [
 
