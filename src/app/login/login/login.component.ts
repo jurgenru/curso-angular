bdc2c561fac92780ac39796035e78eda9079b1ad
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-08-31 15:51:46 -0400

    formularios orientados a template

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -9,9 +9,23 @@ export class LoginComponent implements OnInit {
 
   sw = true;
 
+  username = '';
+  password = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onLogin(): void{
+    console.log('USERNAME: ', this.username);
+    console.log('PASSWORD: ', this.password);
+  }
+
+  onLogin2(form): void{
+    console.log('Variables: ', form.value);
+  }
+
+
 
 }
