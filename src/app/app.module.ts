1653a274194895d34048f30b922dc0ba5454cf6f
jurgenru <jurgenruegnberg@gmail.com>
2020-09-04 17:23:16 -0400

    all wrong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Dish1Component } from './MODULESEX/DishModule/dish/components/dish1/dish1.component';
-import { Dish2Component } from './MODULESEX/DishModule/dish/components/dish2/dish2.component';
-import { DishModule } from './MODULESEX/DishModule/dish/dish.module';
-import { ProviderModule } from './MODULESEX/ProviderModule/provider/provider.module';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRouting } from './app-routing.module';
 import { ConcertComponent } from './concert/concert/concert.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -21,13 +18,13 @@ import { ConcertComponent } from './concert/concert/concert.component';
    ],
   imports: [
     BrowserModule,
-    DishModule,
-    ProviderModule,
+    BrowserAnimationsModule,
     FormsModule,
+    AppRouting,
     NgbModule,
+
     MatSliderModule,
-    MatIconModule,
-    AppRouting
+    MatIconModule
   ],
   bootstrap: [AppComponent]
 })
