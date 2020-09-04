563a7a19d214f5812c22938e02f58cebf3bd3b81
jurgenru <jurgenruegnberg@gmail.com>
2020-09-03 23:17:09 -0400

    home ondestroy

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,7 +7,7 @@ import {Subscription} from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   productSubs: Subscription;
 
